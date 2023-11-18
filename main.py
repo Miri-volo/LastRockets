daa1cf116efd621dd5bc6819b024e7b3df37c455
miri volozhinsky <mirivol@post.bgu.ac.il>
2023-11-18 17:47:37 +0200

    Remove duplicates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
 
 from_date = '07.10.2023'
 
@@ -8,13 +9,18 @@ def get_cities():
     url = "https://www.oref.org.il/Shared/Ajax/GetCitiesMix.aspx?lang=he"
     request = requests.get(url)
     jsonFormat = request.json()
-    cities = []
+    cities = set()
+    area = {}
     for item in jsonFormat:
-        cities.append(item['label_he'])
-    return cities
+        cities.add(item['label_he'])
+        soup = BeautifulSoup(item['mixname'], 'html.parser')
+        value = soup.find('span').text
+        area[item['label_he']] =value
+
+    return cities, area
 
 
-def get_data(cities):
+def get_data(cities, area):
     data_list = []
     for city in cities:
         url = 'https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=he&fromDate=' + from_date + '&toDate=&mode=0&city_0=' + city
@@ -27,13 +33,13 @@ def get_data(cities):
                     data_values = item['data'].split(', ')
                     for data_value in data_values:
                         data_list.append({'alertDate': item['alertDate'], 'date': item['date'], 'time': item['time'],
-                                          'city': data_value})
+                                          'city': data_value, 'area': area[city]})
                 else:
-                    data_list.append({'alertDate': item['alertDate'], 'date': item['date'], 'time': item['time'], 'city': item['data']})
+                    data_list.append({'alertDate': item['alertDate'], 'date': item['date'], 'time': item['time'], 'city': item['data'], 'area': area[city]})
     df = pd.DataFrame(data_list)
     df.to_excel('lastRockets.xlsx', index=False)
 
 
 if __name__ == '__main__':
-    cities = get_cities()
-    get_data(cities)
+    cities, area = get_cities()
+    get_data(cities, area)
